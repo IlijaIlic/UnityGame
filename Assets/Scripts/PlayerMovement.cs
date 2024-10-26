00483361f38762f5d844b134ece87b8949366d4d
Ilija Ilic <ilija.ilic@elfak.rs>
2024-10-26 10:53:52 +0200

    Added to GitHub

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float jumpForce = 10f;
+    public float gravity = 10f;
+    public RigidBody rb;
+   
+    void FixedUpdate()
+    {
+        
+    }
+}
